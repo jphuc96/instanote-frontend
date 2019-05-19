6e113520b7061c3e6734ea9004faa59ec6e8680c
Hoang Phuc Nguyen <jphuc96@gmail.com>
2019-05-20 02:06:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    const responseGoogle = response => {
+      console.log(response);
+    }
+
+    return (
+     <div className= "App">
+       <GoogleLogin
+          clientID="375537975084-497dalo3j1djjaq7kdm8m5d674hrjspn.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          // onSuccess={responseGoogle}
+          // onFailure={responseGoogle}
+        /> 
+     </div>
+    );
+    // return (
+    //   responseGoogle
+    // );
+  }
 }
 
 export default App;
